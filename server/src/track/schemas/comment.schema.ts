dd38eb30c5acb71777b112d144e249cd23088de2
Vpar7907 <108019245+Vpar7907@users.noreply.github.com>
2022-10-18 22:47:34 +0300

    add database

---
+++ server/src/track/schemas/comment.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type CommentDocument = Comment & Document;
+
+@Schema()
+export class Comment {
+  @Prop()
+  username: string;
+
+  @Prop()
+  text: string;
+
+  @Prop()
+  track: string;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
